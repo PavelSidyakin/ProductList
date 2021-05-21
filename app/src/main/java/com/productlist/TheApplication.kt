57623ba2d161547c2b6b7557d0f82213955c30e2
Pavel Sidyakin <p.sidyakin@mylivn.com>
2021-05-21 19:06:37 +0200

    initial project structure

---
+++ app/src/main/java/com/productlist/TheApplication.kt
@@ -0,0 +1,13 @@
+package com.productlist
+
+import android.app.Application
+
+internal class TheApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // TODO: glue modules
+    }
+
+}
