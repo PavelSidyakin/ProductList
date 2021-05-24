bb5fc9f0376953040f4ce02cf53c719e51a47c09
Pavel Sidyakin <p.sidyakin@mylivn.com>
2021-05-24 11:17:38 +0200

    rollback unexpected changes, unused import

--- feature/product_ui/src/main/java/com/productlist/product_ui/impl/mvi/product_list/store/ProductListStore.kt
+++ feature/product_ui/src/main/java/com/productlist/product_ui/impl/mvi/product_list/store/ProductListStore.kt
@@ -1,7 +1,6 @@
 package com.productlist.product_ui.impl.mvi.product_list.store
 
 import com.arkivanov.mvikotlin.core.store.Store
-import com.productlist.product_ui.impl.mvi.product_details.store.ProductDetailsStateChanges
 import com.productlist.product_ui.impl.mvi.product_list.view.recycler.ProductListItem
 
 /**
